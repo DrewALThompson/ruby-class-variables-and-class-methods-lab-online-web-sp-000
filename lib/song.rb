641e3e34a1839c880062a2d7eb0c0aee975edb8d
Drew Thompson <drewallynthompson@gmail.com>
2020-09-04 22:54:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song 
+  
+@@count = 0
+@@genres= []
+@@artists= []
+
+attr_accessor :name, :artist, :genre
+
+  def initialize(song)
+    @song = song
+    @artist = artist
+    @genre = artist
+    @@genres << genre 
+    @@artists << artist 
+    @@count +=1
+  end
+
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    GENRES
+  end
+  
+
+end
